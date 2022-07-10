1dd00d07f7eee4ff3885f69dd908aefbc3583b45
Roberta Benassi <roberta.benassi@gmail.com>
2022-07-10 17:58:20 +0200

    change package in proto

--- internal/transport/ports-service_grpc.pb.go
+++ internal/transport/ports-service_grpc.pb.go
@@ -11,7 +11,7 @@ import (
 	grpc "google.golang.org/grpc"
 	codes "google.golang.org/grpc/codes"
 	status "google.golang.org/grpc/status"
-	data_model "internal/data_model"
+	data_model "microservice-exercise/internal/data_model"
 )
 
 // This is a compile-time assertion to ensure that this generated file
